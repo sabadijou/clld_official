b942fd859b98787c4592fc83d9f378dc824ba910
sabadijou <s.abadijou@gmail.com>
2023-02-20 14:45:37 +0330

    complete

--- models/clos.py
+++ models/clos.py
@@ -5,21 +5,25 @@ import torch
 
 
 class CLoS(nn.Module):
-    def __init__(self):
+    def __init__(self, encoder, cfg):
         super(CLoS, self).__init__()
+        self.cfg = cfg
         self.moment_iter = 0
-        self.moment =
-        self.T =
 
-        ############################################
+        self.moment = self.cfg.training_parameters['encoder_momentum']
+        self.T = self.cfg.training_parameters['temperature']
 
-        self.base_encoder = encoder(dim1=dim1, dim2=dim2)
-        self.moment_encoder = encoder(dim1=dim1, dim2=dim2)
-        self.ppm_module = PPM(input_channels=,
-                              out_channels=)
-        ##########################################
+        self.base_encoder = encoder(dim1=self.cfg.encoder['latent_dims_1'],
+                                    dim2=self.cfg.encoder['latent_dims_2'])
 
-        for param_base, param_moment in zip(self.base_encoder.parameters(), self.moment_encoder.parameters()):
+        self.moment_encoder = encoder(dim1=self.cfg.encoder['latent_dims_1'],
+                                      dim2=self.cfg.encoder['latent_dims_2'])
+
+        self.ppm_module = PPM(input_channels=self.cfg.ppm_module['input_channels'],
+                              out_channels=self.cfg.ppm_module['output_channels'])
+
+        for param_base, param_moment in zip(self.base_encoder.parameters(),
+                                            self.moment_encoder.parameters()):
             param_moment.data.copy_(param_base.data)
             param_moment.requires_grad = False
 
@@ -31,15 +35,17 @@ class CLoS(nn.Module):
         with torch.no_grad():
             self._momentum_update()
             y_prime = self.moment_encoder(view_2)
-            self._momentum_scaling ()
+            self._momentum_scaling()
 
         return y, y_prime, base
 
     @torch.no_grad()
     def _momentum_update(self):
-        for param_base, param_moment in zip (self.base_encoder.parameters (), self.moment_encoder.parameters ()):
+        for param_base, param_moment in zip(self.base_encoder.parameters(), self.moment_encoder.parameters()):
             param_moment.data = param_moment.data * self.moment + param_base.data * (1. - self.moment)
 
     def _momentum_scaling(self):
-        self.moment += (np.sin(np.pi / 2 * self.moment_iter / cfg)) * (1 - self.moment)
+        self.moment += (np.sin(np.pi / 2 *
+                               self.moment_iter / self.cfg.training_parameters['num_epochs']))\
+                       * (1 - self.moment)
         self.moment_iter += 1
