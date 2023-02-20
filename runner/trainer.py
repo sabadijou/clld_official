f6c0d83e0554e8734abbbab96fb45c6cbebbf1d3
sabadijou <s.abadijou@gmail.com>
2023-02-20 14:13:18 +0330

    init

---
+++ runner/trainer.py
@@ -0,0 +1,15 @@
+
+
+class Trainer:
+    def __init__(self, cfg):
+        super(Trainer, self).__init__()
+
+    def main_worker(self, gpus_per_node, cfg):
+        pass
+
+    def train_step(self):
+        pass
+
+    def lr_scheduler(self):
+        pass
+
